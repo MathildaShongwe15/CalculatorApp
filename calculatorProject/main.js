ed385685a5b10f7416adf7aa4a3307af63999c45
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-11-12 16:16:38 +0200

    Fixed styling and functionality

--- calculatorProject/main.js
+++ calculatorProject/main.js
@@ -4,17 +4,34 @@ const number =document.querySelectorAll(".numbers").forEach(function(item){
 item.addEventListener("click", function(e){
     if(val.innerText === "NaN"){
         val.innerText = "";
-
     }
-    if(inputValue.innerText === "0"){
+    if(val.innerText === "0"){
         val.innerText="";
     }
     val.innerText += e.target.innerHTML.trim();
   });
 });
 
-const calculate =document.querySelectorAll(".operations").forEach(function(item){
+const calculate = document.querySelectorAll(".operations").forEach(function(item){
         item.addEventListener("click", function(e){
-        console.log(e.target.innerHTML);
-  })
-})
+            let lastVal =val.innerText.substring(val.innerText.length,val.innerText.length - 1);
+
+            if(!isNaN(lastVal)&& e.target.innerHTML === "="){
+                val.innerText = eval(val.innerText);
+            }
+            else if(e.target.innerHTML === "AC"){
+                val.innerText=0;
+            }
+            else if(e.target.innerHTML === "DEL"){
+                val.innerText = val.innerText.substring(0, val.innerText.length - 1);
+                if(val.innerText.length == 0){
+                    val.innerText =0;
+                }else{
+                    if(!isNaN(lastVal)){
+                        val.innerText += e.target.innerHTML;
+                    }
+                }
+            }
+        });
+});
+
