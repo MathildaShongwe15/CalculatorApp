58ed8c2b0c7522bb5d0b3b93ff5781e08c251db4
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-11-14 15:12:16 +0200

    Functionality fixed

--- calculatorProject/main.js
+++ calculatorProject/main.js
@@ -14,24 +14,27 @@ item.addEventListener("click", function(e){
 
 const calculate = document.querySelectorAll(".operations").forEach(function(item){
         item.addEventListener("click", function(e){
-            let lastVal =val.innerText.substring(val.innerText.length,val.innerText.length - 1);
+           // console.log(e.target.innerHTML);
 
-            if(!isNaN(lastVal)&& e.target.innerHTML === "="){
+     let lastVal =val.innerText.substring(val.innerText.length,val.innerText.length - 1);
+
+            if(!isNaN(lastVal) && e.target.innerText === "="){
                 val.innerText = eval(val.innerText);
             }
-            else if(e.target.innerHTML === "AC"){
-                val.innerText=0;
-            }
-            else if(e.target.innerHTML === "DEL"){
-                val.innerText = val.innerText.substring(0, val.innerText.length - 1);
-                if(val.innerText.length == 0){
-                    val.innerText =0;
-                }else{
-                    if(!isNaN(lastVal)){
-                        val.innerText += e.target.innerHTML;
-                    }
+            else if (e.target.innerHTML === "AC") {
+                val.innerText = 0;
+            } else if (e.target.innerHTML === "DEL") {
+                val.innerText = val.innerText.substring(0,val.innerText.length - 1);
+                if (val.innerText.length == 0) {
+                    val.innerText = 0;
                 }
             }
+            else{
+                if (!isNaN(lastVal)) {
+                    val.innerText += e.target.innerHTML;
+                }
+            }
+
         });
 });
 
